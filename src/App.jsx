48a5873731969a6da79219fac182503d1f876b42
Lime-oss-hash <yozamono@gmail.com>
2025-12-01 20:40:36 +1300

    Initialize core application structure with global styles and main layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css'
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
 
 function App() {
-  return<>
-  
-  </>
-
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Contact />
+      <p>&copy; {new Date().getFullYear()} Sam Bai. All rights reserved</p>
+    </>
+  );
 }
 
-export default App
+export default App;
